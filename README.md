9e3b600ba598aa5da961fd09422b36df0a21bd70
Peter Bjerg Lidegaard <dbf.black.bull@gmail.com>
2025-08-02 16:30:28 +0200

    Added default-settings.lua that handles initialization after all modules have been read and registered.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 >
 > Install instructions for ShaguTweaks can be found on the [GitHub Page](https://github.com/shagu/ShaguTweaks).
 
-1. Download **[Latest Version](https://github.com/shagu/ShaguTweaks-template/archive/master.zip)**
+1. Download **[Latest Version](https://github.com/DBFBlackbull/ShaguTweaks-DBFBlackbull/archive/master.zip)**
 2. Unpack the Zip file
 3. Rename the folder "ShaguTweaks-DBFBlackbull" to "ShaguTweaks-DBFBlackbull"
 4. Copy "ShaguTweaks-DBFBlackbull" into Wow-Directory\Interface\AddOns
---
+++ default-settings.lua
@@ -0,0 +1,35 @@
+local T = ShaguTweaks.T
+
+-- My preferred default settings
+local defaultSettings = {
+	[T["Auto Dismount"]] = false,
+	[T["Auto Stance"]] = false,
+	[T["Cooldown Numbers"]] = false,
+	[T["Item Rarity Borders"]] = false,
+	[T["MiniMap Tweaks"]] = false,
+	[T["Nameplate Class Colors"]] = false,
+	[T["Real Health Numbers"]] = false,
+	[T["Sell Junk"]] = false,
+	[T["Tooltip Details"]] = false,
+	[T["Vendor Values"]] = false,
+
+	[T["Free Slots Count"]] = true,
+	[T["Quick Actions"]] = true,
+	[T["Reagent Counter"]] = true,
+	[T["Show Energy Ticks"]] = true,
+	[T["Unit Frame Big Health"]] = true,
+}
+for modTitle, enabled in pairs(defaultSettings) do
+	ShaguTweaks.mods[modTitle].enabled = enabled
+end
+
+-- UI Changes
+local modsToGiveCategory = {
+	[T["Reduced Actionbar Size"]] = T["Action Bar"],
+	[T["Nameplate Castbar"]] = T["Nameplates"],
+	[T["Nameplate Class Colors"]] = T["Nameplates"],
+	[T["Nameplate Scale"]] = T["Nameplates"]
+}
+for modTitle, category in pairs(modsToGiveCategory) do
+	ShaguTweaks.mods[modTitle].category = category
+end
